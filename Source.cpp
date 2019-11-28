6e374360897496b6913d24664c9c313fef902c34
cdavisj <cdavisj@protonmail.com>
2019-11-27 16:39:47 -0800

    struct-class update

--- Source.cpp
+++ Source.cpp
@@ -4,17 +4,36 @@ using namespace std;
 
 //Write a program which allows the user to decide how big of an array he wants to make(use new command).Then, access the array using pointer arithmetic(not []).Allow him to enter student grades to the array.Then print the array contents.Print the average grade.Do this by writing separate function such as printArrayand calculateAverage.In these functions you will pass the array as a parameter(i.e. using*).
 
-// changes are being made to the file..
-
-// adding another change
-
-struct Student
+class Student
 {
-	int* gradesArray = nullptr;
-	int size{};
-	double averageGrade{};
+public:
+	int* gradesArray;
+	int size;// {} = member intializer = initializing to 0
+	double averageGrade;
 	//int count = 0;
+
+	Student(); // constructor
+	Student(int s);
 };
+
+Student::Student()
+	: size(0), averageGrade(0.0), gradesArray(nullptr) // member initializer list
+{
+	// what I want to be done when an instance of Student has been created
+	std::cout << "Inside default constructor.\n\n";
+}
+
+/*
+	overloading a function is when you create another function of the same name
+	with different parameters
+*/
+
+Student::Student(int s) // overloaded constructor 
+	: size(s), averageGrade(0.0), gradesArray(nullptr)
+{
+	std::cout << "Inside overloaded constructor.\n\n";
+}
+
 
 int getSizeOfArray(Student* temp)
 {
@@ -37,10 +56,10 @@ void enterGrades(Student* temp)
 
 	temp->gradesArray = new int[temp->size];
 
-	for (int i = 0; i < temp->size; i++)
+	for(int i = 0; i < temp->size; i++)
 	{
 		cout << "Enter grade #" << i + 1 << ": ";
-		cin >> *(temp->gradesArray+i);
+		cin >> *(temp->gradesArray + i);
 	}
 
 }
@@ -48,11 +67,11 @@ void enterGrades(Student* temp)
 void printGradeArray(Student temp)
 {
 	cout << "Here are your " << temp.size << " total grades: ";
-	for (int i = 0; i < temp.size; i++)
+	for(int i = 0; i < temp.size; i++)
 	{
-		cout << *(temp.gradesArray+i);
+		cout << *(temp.gradesArray + i);
 
-		if (i == temp.size - 1)
+		if(i == temp.size - 1)
 		{
 			cout << " ";
 			break;
@@ -69,10 +88,10 @@ int getAverageGrade(Student* temp)
 {
 	int gradeSum = 0;
 
-	for (int i = 0; i < temp->size; i++)
+	for(int i = 0; i < temp->size; i++)
 	{
-		gradeSum = gradeSum + *(temp->gradesArray+i);
-								//temp->gradesArray[i]
+		gradeSum = gradeSum + *(temp->gradesArray + i);
+		//temp->gradesArray[i]
 	}
 
 	cout << "Your average Grade is: ";
@@ -87,7 +106,10 @@ int getAverageGrade(Student* temp)
 
 void main()
 {
-	Student temp;
+	Student temp; // this is where the default constructor is called
+
+	int size = 10;
+	Student s0(size); // this is calling the overloaded vers. of the Student constructor
 
 	temp.size = getSizeOfArray(&temp); //get size of array from student input
 
